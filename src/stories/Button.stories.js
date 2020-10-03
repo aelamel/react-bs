d0a14f35bd797a97d57c28a8486f612ceb10c2d6
maniagga <maniagga@adobe.com>
2020-10-03 20:11:47 +0530

    Button from components added in storybook

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,36 +1,17 @@
 import React from 'react';
-
-import { Button } from './Button';
+import  Button  from '../components/Button';
 
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Button {...args}/>;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  text: "Hello",
+  onClick: ()=>{console.log("hello button clicked");
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
