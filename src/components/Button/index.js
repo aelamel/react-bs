80c07c44ec233385c96b934303288bb6fbbe9834
Karan Shinde <karan.shinde@payu.in>
2020-10-04 03:15:43 +0530

    Added Primary Button Style to Button Component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ButtonStyle = {
+  backgroundColor : '#46a049',
+  color: '#fff',
+  padding: '5px 25px',
+  borderRadius: '2px',
+  fontSize:'16px',
+  border: '1px solid',
+  cursor:'pointer',
+}
+
 const Button = ({text, onClick}) => {
 
     return (
-      <button type="button" onClick={onClick}>
+      <button type="button" style={ButtonStyle} onClick={onClick}>
         {text}
       </button>
     );
