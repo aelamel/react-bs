726c10ce50781f01b2ae1ab37bd7fee16bd3a177
Ruan Victor <ruan.techno@gmail.com>
2020-10-27 16:56:26 -0300

    [#32] Add table component

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+const defaultProps = {
+    options:
+        {
+            style : ''
+        },
+};
+
+const Table = ({options}) => {
+
+    return(
+        <div>
+            <table className={ "table " + options.style}  >
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody> 
+            </table>
+        </div>
+    );
+};
+
+Table.defaultProps = defaultProps;
+
+export default Table;
+
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Button} from './Button';
 export {default as Dropdown} from './Dropdown';
 export {default as Checkbox} from './Checkbox';
+export {default as Table} from './Table';
