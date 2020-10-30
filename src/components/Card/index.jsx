3c5258560a0fce6fc43fb21b058c968fc9497fd8
Chabli Mohammed Yassine <mychabli@octo.com>
2020-10-30 17:12:21 +0100

    adding card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,50 @@
+import React , {Fragment}  from "react";
+import PropTypes from 'prop-types';
+import preview from '../../assets/images/image_preview.png';
+
+
+
+const defaultProps = {
+    img : '',
+    buttonLabel: 'Go somewhere',
+    content: ' Some quick example text to build on the card title and make up the bulk of the cards content. ',
+    title : 'Card title',
+    options : {
+        style : ' ',
+    }
+};
+
+
+const Card = ({ img, title ,content , buttonLabel , onClick , options }) => {
+
+
+    options = {...Card.defaultProps.options, ...options };
+    img = (img) ? img : preview;
+
+    return(
+        <Fragment>
+            <div className={"card " + options.style} >
+                <img className="card-img-top" src={img} alt={img}/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{content}</p>
+                        <a href="#" className="btn btn-primary" onClick={onClick}>{buttonLabel}</a>
+                    </div>
+            </div>
+        </Fragment>
+    );
+
+};
+
+
+Card.prototypes = {
+    img : PropTypes.string,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    buttonLabel: PropTypes.string,
+    onclick: PropTypes.func,
+    options : PropTypes.object
+    };
+
+Card.defaultProps = defaultProps;
+export default Card;
